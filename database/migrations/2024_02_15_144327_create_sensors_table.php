c210b1b1f8231b86c3e1888a88deb65d9a1cced1
Iraic <joseiraic@gmail.com>
2024-02-15 15:01:24 -0600

    Initial commit

---
+++ database/migrations/2024_02_15_144327_create_sensors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+            $table->string("type"); // tipo de sesnor
+            $table->decimal("value", 10, 2); // valor del sensor
+            $table->datetime("date"); // fecha y hora de la lectura
+            $table->string("user"); // usuario que realizo la lectura
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensors');
+    }
+};
